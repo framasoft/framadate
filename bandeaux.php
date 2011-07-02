894b526effa22288fec4be4845869f95390fdff2
Simon Leblanc <contact@leblanc-simon.eu>
2011-07-02 23:13:56 +0200

    Fix issue #11 : Google Analytics n'est plus obligatoire

--- bandeaux.php
+++ bandeaux.php
@@ -51,21 +51,21 @@ function framanav()
 }
 
 function gAnalytics() {
-echo <<<gAnalytics
+  if (GOOGLE_ANALYTICS_ID !== false) {
+    echo '
 <script type="text/javascript">
-
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-896440-11']);
-  _gaq.push(['_trackPageview']);
+  _gaq.push([\'_setAccount\', \''.GOOGLE_ANALYTICS_ID.'\']);
+  _gaq.push([\'_trackPageview\']);
 
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
-</script>
-gAnalytics;
+</script>';
+  }
 }
 
 
