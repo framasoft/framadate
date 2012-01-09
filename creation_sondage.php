9722388195d27a1efb34a4c94ccd5b5d5a1d0fa1
Jean-Philippe Guérard <jean-philippe.guerard@tigreraye.org>
2012-01-09 02:37:39 +0100

    Fix names with accents in the user mail

--- creation_sondage.php
+++ creation_sondage.php
@@ -108,7 +108,7 @@ function ajouter_sondage()
   
   $message = _("This is the message you have to send to the people you want to poll. \nNow, you have to send this message to everyone you want to poll.");
   $message .= "\n\n";
-  $message .= stripslashes(htmlspecialchars_decode($_SESSION["nom"],ENT_QUOTES))." " . _("hast just created a poll called") . " : \"".stripslashes(htmlspecialchars_decode($_SESSION["titre"],ENT_QUOTES))."\".\n";
+  $message .= stripslashes(html_entity_decode($_SESSION["nom"],ENT_QUOTES,"UTF-8"))." " . _("hast just created a poll called") . " : \"".stripslashes(htmlspecialchars_decode($_SESSION["titre"],ENT_QUOTES))."\".\n";
   $message .= _("Thanks for filling the poll at the link above") . " :\n\n%s\n\n" . _("Thanks for your confidence") . ",\n".NOMAPPLICATION;
   
   $message_admin = _("This message should NOT be sended to the polled people. It is private for the poll's creator.\n\nYou can now modify it at the link above");
