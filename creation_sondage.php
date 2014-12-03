6751a8a9cfc84f2a49d46ccd2aec2ad0bcd72485
FramaJosephK <josephk@framasoft.org>
2014-12-03 14:30:25 +0100

    Retouches après série de merges

--- creation_sondage.php
+++ creation_sondage.php
@@ -76,12 +76,12 @@ function ajouter_sondage()
     $connect->Execute($sql, array($poll, $_SESSION['toutchoix']));
 
     if($config['use_smtp']==true) {
-        $message = _('This is the message you have to send to the people you want to poll. \nNow, you have to send this message to everyone you want to poll.');
+        $message = _("This is the message you have to send to the people you want to poll. \nNow, you have to send this message to everyone you want to poll.");
         $message .= "\n\n";
         $message .= stripslashes(html_entity_decode($_SESSION["nom"],ENT_QUOTES,"UTF-8"))." " . _('hast just created a poll called') . ' : "'.stripslashes(htmlspecialchars_decode($_SESSION['titre'],ENT_QUOTES))."\".\n";
         $message .= _('Thanks for filling the poll at the link above') . " :\n\n%s\n\n" . _('Thanks for your confidence.') ."\n". NOMAPPLICATION;
 
-        $message_admin = _('This message should NOT be sent to the polled people. It is private for the poll\'s creator.') ."\n\n" ._('You can now modify it at the link above');
+        $message_admin = _("This message should NOT be sent to the polled people. It is private for the poll's creator.\n\nYou can now modify it at the link above");
         $message_admin .= " :\n\n%s \n\n" . _('Thanks for your confidence.') . "\n". NOMAPPLICATION;
 
         $message = sprintf($message, Utils::getUrlSondage($poll));
