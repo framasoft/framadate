133dd203dfb426b0574eecc37ec0c03daa8d96e4
Antonin <zepcome@gmail.com>
2015-03-26 17:12:31 +0100

    Forgot a useless assignment...

--- studs.php
+++ studs.php
@@ -203,6 +203,5 @@ $smarty->assign('editingVoteId', $editingVoteId);
 $smarty->assign('message', $message);
 $smarty->assign('admin', false);
 $smarty->assign('parameter_name_regex', NAME_REGEX);
-$smarty->assign('parameter_name_error', _('Name is incorrect.'));
 
 $smarty->display('studs.tpl');
