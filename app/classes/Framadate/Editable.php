1e39eae05633384b1760525098bcc02c61aa4d20
Antonin <zepcome@gmail.com>
2015-04-06 12:54:18 +0200

    Changed the file header to framadate license

--- app/classes/Framadate/Editable.php
+++ app/classes/Framadate/Editable.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: antonin
- * Date: 05/04/15
- * Time: 14:28
+ * This software is governed by the CeCILL-B license. If a copy of this license
+ * is not distributed with this file, you can obtain one at
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+ *
+ * Authors of STUdS (initial project): Guilhem BORGHESI (borghesi@unistra.fr) and Raphaël DROZ
+ * Authors of Framadate/OpenSondate: Framasoft (https://github.com/framasoft)
+ *
+ * =============================
+ *
+ * Ce logiciel est régi par la licence CeCILL-B. Si une copie de cette licence
+ * ne se trouve pas avec ce fichier vous pouvez l'obtenir sur
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-fr.txt
+ *
+ * Auteurs de STUdS (projet initial) : Guilhem BORGHESI (borghesi@unistra.fr) et Raphaël DROZ
+ * Auteurs de Framadate/OpenSondage : Framasoft (https://github.com/framasoft)
  */
 
 namespace Framadate;
@@ -13,6 +24,7 @@ namespace Framadate;
  * Class Editable
  *
  * Is used to specify the poll's edition permissions.
+ * @TODO : wait to use the SplEnum
  *
  * @package Framadate
  */
