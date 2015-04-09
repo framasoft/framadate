f888a643e06526b44ea38cf1ebc841916c88bb22
Antonin <zepcome@gmail.com>
2015-04-09 12:34:48 +0200

    Added column password in poll table.

--- app/classes/Framadate/Migration/AddColumns_password_hash_And_results_publicly_visible_In_poll_For_0_9.php
+++ app/classes/Framadate/Migration/AddColumns_password_hash_And_results_publicly_visible_In_poll_For_0_9.php
@@ -21,12 +21,12 @@ namespace Framadate\Migration;
 use Framadate\Utils;
 
 /**
- * Class From_0_0_to_0_8_Migration
+ * This migration adds the fields password_hash and results_publicly_visible on the poll table.
  *
  * @package Framadate\Migration
- * @version 0.8
+ * @version 0.9
  */
-class From_0_0_to_0_8_Migration implements Migration {
+class AddColumns_password_hash_And_results_publicly_visible_In_poll_For_0_9 implements Migration {
 
     function __construct() {
     }
@@ -37,7 +37,7 @@ class From_0_0_to_0_8_Migration implements Migration {
      * @return string The description of the migration class
      */
     function description() {
-        return 'First installation of the Framadate application (v0.8)';
+        return 'Add columns "password_hash" and "results_publicly_visible" in table "vote" for version 0.9';
     }
 
     /**
@@ -51,8 +51,8 @@ class From_0_0_to_0_8_Migration implements Migration {
         $stmt = $pdo->query('SHOW TABLES');
         $tables = $stmt->fetchAll(\PDO::FETCH_COLUMN);
 
-        // Check if there is no tables but the MIGRATION_TABLE one
-        $diff = array_diff($tables, [Utils::table(MIGRATION_TABLE)]);
+        // Check if tables of v0.9 are presents
+        $diff = array_diff([Utils::table('poll'), Utils::table('slot'), Utils::table('vote'), Utils::table('comment')], $tables);
         return count($diff) === 0;
     }
 
@@ -63,47 +63,16 @@ class From_0_0_to_0_8_Migration implements Migration {
      * @return bool true is the execution succeeded
      */
     function execute(\PDO $pdo) {
-        $pdo->exec('
-CREATE TABLE IF NOT EXISTS `sondage` (
-  `id_sondage` char(16) NOT NULL,
-  `commentaires` text,
-  `mail_admin` varchar(128) DEFAULT NULL,
-  `nom_admin` varchar(64) DEFAULT NULL,
-  `titre` text,
-  `id_sondage_admin` char(24) DEFAULT NULL,
-  `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_fin` timestamp NOT NULL DEFAULT \'0000-00-00 00:00:00\',
-  `format` varchar(2) DEFAULT NULL,
-  `mailsonde` tinyint(1) DEFAULT \'0\',
-  `statut` int(11) NOT NULL DEFAULT \'1\' COMMENT \'1 = actif ; 0 = inactif ; \',
-  UNIQUE KEY `id_sondage` (`id_sondage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+        $this->alterPollTable($pdo);
 
-        $pdo->exec('
-CREATE TABLE IF NOT EXISTS `sujet_studs` (
-  `id_sondage` char(16) NOT NULL,
-  `sujet` text,
-  KEY `id_sondage` (`id_sondage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
-
-        $pdo->exec('
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id_comment` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_sondage` char(16) NOT NULL,
-  `comment` text NOT NULL,
-  `usercomment` text,
-  PRIMARY KEY (`id_comment`),
-  KEY `id_sondage` (`id_sondage`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;');
-
-        $pdo->exec('
-CREATE TABLE IF NOT EXISTS `user_studs` (
-  `id_users` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nom` varchar(64) NOT NULL,
-  `id_sondage` char(16) NOT NULL,
-  `reponses` text NOT NULL,
-  PRIMARY KEY (`id_users`),
-  KEY `id_sondage` (`id_sondage`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;');
+        return true;
     }
+
+    private function alterPollTable(\PDO $pdo) {
+        $pdo->exec('
+        ALTER TABLE `' . Utils::table('poll') . '`
+        ADD `password_hash` VARCHAR(255) NULL DEFAULT NULL ,
+        ADD `results_publicly_visible` TINYINT(1) NULL DEFAULT NULL');
+    }
+
 }
