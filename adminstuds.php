50f3088422d810795a444f8f131b6217bf4d9596
Olivier PEREZ <olivier@olivierperez.fr>
2015-05-29 19:10:39 +0200

    Little fix for previous merge

--- adminstuds.php
+++ adminstuds.php
@@ -422,7 +422,7 @@ $smarty->assign('admin_poll_id', $admin_poll_id);
 $smarty->assign('poll', $poll);
 $smarty->assign('title', __('Generic', 'Poll') . ' - ' . $poll->title);
 $smarty->assign('expired', strtotime($poll->end_date) < time());
-$smarty->assign('deletion_date', $poll->end_date + PURGE_DELAY * 86400);
+$smarty->assign('deletion_date', strtotime($poll->end_date) + PURGE_DELAY * 86400);
 $smarty->assign('slots', $poll->format === 'D' ? $pollService->splitSlots($slots) : $slots);
 $smarty->assign('votes', $pollService->splitVotes($votes));
 $smarty->assign('best_choices', $pollService->computeBestChoices($votes));
