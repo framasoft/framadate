75d0649c15fbb3f4a346e1e36a7924f97a1e341b
Maxime <maxoo.design@free.fr>
2015-06-05 16:42:25 +0200

    use require instead of include* functions:  - the script must be stopped when the constants.php file is missing

--- app/inc/init.php
+++ app/inc/init.php
@@ -21,10 +21,10 @@ if (ini_get('date.timezone') == '') {
     date_default_timezone_set('Europe/Paris');
 }
 // Autoloading of dependencies with Composer
-require_once __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-include_once __DIR__ . '/constants.php';
-include_once __DIR__ . '/i18n.php';
+require __DIR__ . '/constants.php';
+require __DIR__ . '/i18n.php';
 
 $connect = NewADOConnection(BASE_TYPE);
 $connect->Connect(SERVEURBASE, USERBASE, USERPASSWD, BASE);
