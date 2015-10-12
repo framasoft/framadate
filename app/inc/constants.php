de18f826f0f3eb477ebe448f01dae7f119a84980
Antonin <antonin.murtin@gmail.com>
2015-10-12 21:26:13 +0200

    The name can be any value.
    There is no reason to limit the caracters that can be used in the name.
    Thus, removing the regex.
    
    Fix #97

--- app/inc/constants.php
+++ app/inc/constants.php
@@ -23,7 +23,6 @@ const VERSION = '0.9';
 // Regex
 const POLL_REGEX = '/^[a-z0-9]+$/i';
 const CHOICE_REGEX = '/^[012]$/';
-const NAME_REGEX = '/^[áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœa-z0-9_ -]+$/i';
 const BOOLEAN_REGEX = '/^(on|off|true|false|1|0)$/i';
 const BOOLEAN_TRUE_REGEX = '/^(on|true|1)$/i';
 const EDITABLE_CHOICE_REGEX = '/^[0-2]$/';
