11ff60a170d3314c04447bcc36824f4b5c29edab
Olivier Perez <olivier@olivierperez.fr>
2016-04-27 01:01:23 +0200

    Fix CHOICE_REGEX to allow space

--- app/inc/constants.php
+++ app/inc/constants.php
@@ -22,7 +22,7 @@ const VERSION = '0.9';
 
 // Regex
 const POLL_REGEX = '/^[a-z0-9]+$/i';
-const CHOICE_REGEX = '/^[012]$/';
+const CHOICE_REGEX = '/^[ 012]$/';
 const BOOLEAN_REGEX = '/^(on|off|true|false|1|0)$/i';
 const BOOLEAN_TRUE_REGEX = '/^(on|true|1)$/i';
 const EDITABLE_CHOICE_REGEX = '/^[0-2]$/';
