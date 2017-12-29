028a354a45ebb7fe39c61b1d091f62a20b01e1ba
mysteriou <elio007@hotnail.fr>
2017-12-29 12:51:20 +0100

    ajout change permission  de tout les fichier dans ./install.php

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
+# En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
+
+chmod -R 777 ./
+
+exit 0
+
+
