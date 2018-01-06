9d8741e5c7440baae45045bd32403c2f0530465a
mysteriou <elio007@hotnail.fr>
2018-01-06 11:24:44 +0100

    ajout verif la case base du tableau fields est pas vide dans la fonction install dans la class InstallService

--- app/classes/Framadate/Services/InstallService.php
+++ app/classes/Framadate/Services/InstallService.php
@@ -137,7 +137,7 @@ return $pass = crypt($Pass);
 
     public function install(Smarty &$smarty) {
         // Check values are present
-        if (empty($this->fields['appName']) || empty($this->fields['appMail']) || empty($this->fields['defaultLanguage']) || empty($this->fields['dataname']) || empty($this->fields['dbUser'])  || empty($this->fields['server'])) {
+        if (empty($this->fields['appName']) || empty($this->fields['appMail']) || empty($this->fields['defaultLanguage']) || empty($this->fields['dataname']) || empty($this->fields['dbUser'])  || empty($this->fields['server']) || empty($this->fields['base']) ) {
             return $this->error('MISSING_VALUES');
         }
 
