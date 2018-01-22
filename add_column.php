accc97ddc8d3853c6a794174498c179ea6732574
mysteriou <elio007@hotnail.fr>
2018-01-22 15:14:52 +0100

    initialisation de la class SlotRepository

--- add_column.php
+++ add_column.php
@@ -1,6 +1,16 @@
 <?php 
 
+namespace Framadate\Repositories;
+
+use Framadate\Services\LogService;
+
+use Framadate\FramaDB;
+use Framadate\Utils;
+
+
 include_once __DIR__ . '/app/inc/init.php';
 
+$logService = new LogService();
+$SlotRepository = new SlotRepository($connect, $logService);
 
 ?>
