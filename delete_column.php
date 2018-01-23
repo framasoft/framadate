2035380b71bf2cdab62c597cef9dcd7e6de7a164
mysteriou <elio007@hotnail.fr>
2018-01-23 19:08:27 +0100

    include SlotRepository et ses dependance dans ./delete_column.php

--- delete_column.php
+++ delete_column.php
@@ -1,5 +1,16 @@
 <?php 
 
+namespace Framadate\Repositories;
+
+use Framadate\FramaDB;
+use Framadate\Utils;
+use Framadate\Services\LogService;
+
 include_once __DIR__ . '/app/inc/init.php';
+
+$logService = new LogService();
+
+$SlotRepository = new SlotRepository($connect, $logService);
+
 
 ?>
