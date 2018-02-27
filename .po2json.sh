6839065ece251f957e214e88b106aac571ce944d
Luc Didry <luc@didry.org>
2018-02-27 17:10:34 +0100

    [Zanata] Fix bugs in po to nested json toolchain

--- .po2json.sh
+++ .po2json.sh
@@ -3,5 +3,5 @@ for i in po/*.po
 do
     j=$(echo $i | cut -d '.' -f 1 | cut -d '/' -f 2)
     po2json -i $i -t locale/en.json --progress none | ./.renest_json.pl > po/$j.json
-    mv po/$j.json locale/$j.json
 done
+mv po/*.json locale/
