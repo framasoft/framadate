54ed0a218addc24cf6e4ee096d5812bc53c5cac6
Thomas Citharel <tcit@tcit.fr>
2018-03-20 10:50:53 +0100

    show error on failure
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- tests/Controllers/PollControllerTest.php
+++ tests/Controllers/PollControllerTest.php
@@ -8,10 +8,9 @@ class PollControllerTest extends FramaWebTestCase
 {
     public function testHomePage()
     {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/');
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $this->assertCount(1, $crawler->filter('h2:contains("Generic.Make your polls")'));
 
         $this->assertCount(3, $crawler->filter('p.home-choice'));
@@ -19,14 +18,13 @@ class PollControllerTest extends FramaWebTestCase
 
     public function testDatePollForm()
     {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/');
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $datePollButton = $crawler->filter('p.home-choice a')->first()->link();
-        $crawler = $client->click($datePollButton);
+        $crawler = $this->client->click($datePollButton);
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $this->assertCount(1, $crawler->filter('form[name="poll"]'));
         $this->assertCount(1, $crawler->filter('div.alert-info:contains("Step 1.You are in the poll creation section.")'));
 
@@ -34,10 +32,9 @@ class PollControllerTest extends FramaWebTestCase
 
     public function testSubmitDatePollForm()
     {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/new/date');
+        $crawler = $this->client->request('GET', '/new/date');
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $this->assertCount(1, $crawler->filter('form[name="poll"]'));
         $this->assertCount(1, $crawler->filter('div.alert-info:contains("Step 1.You are in the poll creation section.")'));
 
@@ -49,11 +46,11 @@ class PollControllerTest extends FramaWebTestCase
             'poll[description]' => 'my awesome poll'
         ];
 
-        $client->submit($form, $data);
+        $this->client->submit($form, $data);
 
-        $this->assertSame(302, $client->getResponse()->getStatusCode());
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
 
-        $crawler = $client->followRedirect();
+        $crawler = $this->client->followRedirect();
 
         $this->assertContains('Step 2.Title', $crawler->filter('body')->extract(['_text'])[0]);
 
@@ -64,21 +61,20 @@ class PollControllerTest extends FramaWebTestCase
             'horaires0' => ['12h', '13h'],
         ];
 
-        $client->submit($form, $data);
+        $this->client->submit($form, $data);
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testClassicPollForm()
     {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/');
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $datePollButton = $crawler->filter('p.home-choice a')->eq(1)->link();
-        $crawler = $client->click($datePollButton);
+        $crawler = $this->client->click($datePollButton);
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($this->client->getResponse()->isOk());
         $this->assertCount(1, $crawler->filter('form[name="poll"]'));
         $this->assertCount(1, $crawler->filter('div.alert-info:contains("Step 1.You are in the poll creation section.")'));
 
--- tests/FramaWebTestCase.php
+++ tests/FramaWebTestCase.php
@@ -2,7 +2,34 @@
 namespace Framadate\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Client;
+use Throwable;
 
 abstract class FramaWebTestCase extends WebTestCase {
 
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function onNotSuccessfulTest(Throwable $exception)
+    {
+        $message = null;
+        if ($this->client->getCrawler()) {
+            $message = preg_replace('#\s{2,}#', '', $this->client->getCrawler()->filter('.text-exception')->text());
+        }
+        if ($message) {
+            $exceptionClass = get_class($exception);
+            throw new $exceptionClass($exception->getMessage() . ' | ' . $message);
+        }
+        throw $exception;
+    }
 }
