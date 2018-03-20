65d8fd9639754a4a721e0d43a3525879f0f77ccd
Luc Didry <luc@didry.org>
2018-03-20 17:09:02 +0100

    [zanata] Use en translation string if not translated

--- .po2json.sh
+++ .po2json.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+po2json -i po/en.po -t locale/en.json --progress none -o po/default.json
+
 for i in po/*.po
 do
     j=$(echo $i | cut -d '.' -f 1 | cut -d '/' -f 2)
     po2json -i $i -t locale/en.json --progress none | ./.renest_json.pl > po/$j.json
+    mv po/$j.json locale/
 done
-mv po/*.json locale/
