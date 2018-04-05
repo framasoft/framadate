1f2e7a422e0ed9ba534a7eb59763d014d8d2b152
m <m>
2018-04-05 16:36:50 +0200

    prepare PollRepository and create_poll

--- create_poll.php
+++ create_poll.php
@@ -22,6 +22,7 @@ use Framadate\Repositories\RepositoryFactory;
 use Framadate\Security\PasswordHasher;
 use Framadate\Services\InputService;
 use Framadate\Utils;
+use Framadate\Repositories\PollRepository;
 
 include_once __DIR__ . '/app/inc/init.php';
 
@@ -100,7 +101,8 @@ if ($goToStep2) {
     $_SESSION['form']->collect_users_mail = $collect_users_mail;
     $_SESSION['form']->use_password = ($use_password !== null);
     $_SESSION['form']->results_publicly_visible = ($results_publicly_visible !== null);
-
+    $_SESSION['form']->vote_type = PollRepository::VOTE_TYPE_3_CHOICES;
+    
     if ($config['use_smtp'] === true && empty($mail)) {
         $error_on_mail = true;
     }
