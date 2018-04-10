1cfeb983fa9c7890b4df3a8f90d3fd9dcf90c34b
m <m>
2018-04-10 13:59:13 +0200

    new InvalidVoteException

---
+++ app/classes/Framadate/Exception/InvalidVoteException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Framadate\Exception;
+
+/**
+ * Class InvalidVoteException
+ *
+ * Thrown when a choice is not valid for the vote type
+ */
+class InvalidVoteException extends \Exception {
+    function __construct() {
+    }
+}
