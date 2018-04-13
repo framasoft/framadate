6455f4be870870ec25b3f5c9212ae70dbe5f15a9
m <m>
2018-04-13 21:24:43 +0200

    nettoyage du sondage de démonstration https://framagit.org/framasoft/framadate/issues/316

---
+++ admin/cleanDemo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This software is governed by the CeCILL-B license. If a copy of this license
+ * is not distributed with this file, you can obtain one at
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+ *
+ * Authors of STUdS (initial project): Guilhem BORGHESI (borghesi@unistra.fr) and Raphaël DROZ
+ * Authors of Framadate/OpenSondage: Framasoft (https://github.com/framasoft)
+ *
+ * =============================
+ *
+ * Ce logiciel est régi par la licence CeCILL-B. Si une copie de cette licence
+ * ne se trouve pas avec ce fichier vous pouvez l'obtenir sur
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-fr.txt
+ *
+ * Auteurs de STUdS (projet initial) : Guilhem BORGHESI (borghesi@unistra.fr) et Raphaël DROZ
+ * Auteurs de Framadate/OpenSondage : Framasoft (https://github.com/framasoft)
+ */
+
+use Framadate\Services\LogService;
+use Framadate\Services\PurgeService;
+
+include_once __DIR__ . '/../app/inc/init.php';
+
+$logService = new LogService();
+$purgeService = new PurgeService($connect, $logService);
+
+$purgeService->cleanDemoPoll();
+
+
