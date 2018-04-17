758d673f0ca1743a296cf73a0a369df6bb50a8ad
Luc Didry <luc@didry.org>
2018-04-17 11:46:15 +0200

    Add script to add a key/value in locale/en.json + put all scripts in scripts/

--- scripts/po2json.sh
+++ scripts/po2json.sh
@@ -4,6 +4,6 @@ po2json -i po/en.po -t locale/en.json --progress none -o po/default.json
 for i in po/*.po
 do
     j=$(echo $i | cut -d '.' -f 1 | cut -d '/' -f 2)
-    po2json -i $i -t locale/en.json --progress none | ./.renest_json.pl > po/$j.json
+    po2json -i $i -t locale/en.json --progress none | scripts/renest_json.pl > po/$j.json
     mv po/$j.json locale/
 done
