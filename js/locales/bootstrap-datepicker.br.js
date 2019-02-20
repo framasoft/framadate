e78ad00d28f1f6043472a3db329e26cbfa8c23d9
Florent Grouin <florent.grouin+frama@gmail.com>
2019-02-20 13:52:07 +0100

    Add new file (breton translation for bootstrap-datepicker)

---
+++ js/locales/bootstrap-datepicker.br.js
@@ -0,0 +1,17 @@
+/**
+ * Breton translation for bootstrap-datepicker
+ * Florent Grouin <florent.grouin+frama@gmail.com>
+ */
+;(function($){
+	$.fn.datepicker.dates['br'] = {
+		days: ["Sul", "Lun", "Meurzh", "Merc'her", "Yaou", "Gwener", "Sadorn", "Sul"],
+		daysShort: ["Sul", "Lun", "Meu", "Mer", "Yao", "Gwe", "Sad", "Sul"],
+		daysMin: ["Su", "L", "Meu", "Mer", "Y", "G", "Sa", "Su"],
+		months: ["Genver", "C'hwevrer", "Meurzh", "Ebrel", "Mae", "Mezheven", "Gouere", "Eost", "Gwengolo", "Here", "Du", "Kerzu"],
+		monthsShort: ["Gen", "C'hw", "Meu", "Ebr", "Mae", "Mezh", "Gou", "Eost", "Gwen", "Here", "Du", "Kerz"],
+		today: "Hiziv",
+		clear: "Dilemel",
+		weekStart: 1,
+		format: "dd/mm/yyyy"
+	};
+}(jQuery));
