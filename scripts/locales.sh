a30098d267856649440cc4750a73f8862731740d
Nicolas Bouilleaud <nico@bou.io>
2019-04-18 15:15:14 +0200

    Remove all scripts for json-based i18n

--- scripts/locales.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python -m json.tool < locale/en.json >/dev/null 2>&1
-if [[ $? == 0 ]];
-then
-    json2po -P -i locale/en.json -t locale/en.json -o po/framadate.pot --duplicates merge
-else
-    echo "Can't convert json files to po, the json file is incorrect"
-    exit 1
-fi
