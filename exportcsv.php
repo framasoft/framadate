ec3ae81e4d99382c07e972a949b12d90040a2278
herve0742 <herv.e@free.fr>
2022-04-11 19:12:33 +0200

    [CSV] Quote columns headers to fix #571.

--- exportcsv.php
+++ exportcsv.php
@@ -92,7 +92,7 @@ if ($poll->format === 'D') {
 } else {
     echo ',';
     foreach ($slots as $slot) {
-        echo Utils::markdown($slot->title, true) . ',';
+        echo Utils::csvEscape(Utils::markdown($slot->title, true)) . ',';
     }
     echo "\r\n";
 }
